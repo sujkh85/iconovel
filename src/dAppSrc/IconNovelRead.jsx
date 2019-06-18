730beb7ab2ae0698e059ff43fcf2eab2386f2f7a
sujkh <sujkh@velic.io>
2019-06-18 19:22:44 +0900

    프리티어 적용

--- src/dAppSrc/IconNovelRead.jsx
+++ src/dAppSrc/IconNovelRead.jsx
@@ -2,107 +2,99 @@ import React, { Component } from 'react';
 import styled from 'styled-components';
 import IconConnect from './components/IconConnect';
 import IconSDK from './components/icon-sdk';
-import {
-  IconConverter
-} from 'icon-sdk-js'
-
+import { IconConverter } from 'icon-sdk-js';
 
 const IconNovelReadWrapper = styled.div`
-  .read-title{
+  .read-title {
     font-size: 20px;
     font-weight: 800;
   }
-  .read-list{
-    margin-top:20px;
-    border:2px solid rgb(0,188,212);
-    border-radius:4px;
+  .read-list {
+    margin-top: 20px;
+    border: 2px solid rgb(0, 188, 212);
+    border-radius: 4px;
   }
-  table{
-    width:100%;
-    border-collapse:collapse;
-    thead{
-      height:40px;
+  table {
+    width: 100%;
+    border-collapse: collapse;
+    thead {
+      height: 40px;
       text-align: center;
-      font-weight:bold;
-      background:#f0f2f5;
-      th{
-        padding:5px;
+      font-weight: bold;
+      background: #f0f2f5;
+      th {
+        padding: 5px;
       }
     }
   }
-  tbody{
+  tbody {
     cursor: pointer;
-    tr{
-      &:hover{
-        background:rgb(238,238,238);
-        font-weight:bold;
+    tr {
+      &:hover {
+        background: rgb(238, 238, 238);
+        font-weight: bold;
       }
     }
-    td{
-      padding:10px;
-      font-size:12px;
+    td {
+      padding: 10px;
+      font-size: 12px;
     }
   }
-`
-
-
+`;
 
 class IconNovelRead extends Component {
-  constructor(props){
-    super(props)
+  constructor(props) {
+    super(props);
     this.state = {
-      list:[]
-    }
+      list: [],
+    };
   }
-  onClickList=(item)=>{
-    this.props.readDetailSelectMenu(item)
-  }
+  onClickList = item => {
+    this.props.readDetailSelectMenu(item);
+  };
 
-  componentDidMount(){
-    this.props.getTextList()
+  componentDidMount() {
+    this.props.getTextList();
   }
 
   render() {
-    const {textList } = this.props
-    return <div>
-      <IconNovelReadWrapper>
-      <div className="read-title">
-         LIST
+    const { textList } = this.props;
+    return (
+      <div>
+        <IconNovelReadWrapper>
+          <div className="read-title">LIST</div>
+          <div className="read-list">
+            <table>
+              <colgroup>
+                <col style={{ width: '30%' }} />
+                <col style={{ width: '70%' }} />
+              </colgroup>
+              <thead>
+                <tr>
+                  <th>address</th>
+                  <th>제목</th>
+                </tr>
+              </thead>
+              <tbody>
+                {textList.map((item, index) => {
+                  return (
+                    <tr
+                      key={index}
+                      onClick={() => {
+                        this.onClickList(item);
+                      }}
+                    >
+                      <td>{item.address}</td>
+                      <td>{item.title}</td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+          </div>
+        </IconNovelReadWrapper>
       </div>
-      <div className="read-list">
-        <table>
-            <colgroup>
-              <col style={{ width: '30%' }} />
-              <col style={{ width: '70%' }} />
-            </colgroup>
-          <thead>
-            <tr>
-              <th>
-                address
-              </th>
-              <th>
-                제목
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-          {textList.map((item, index)=>{
-            return <tr key ={ index} onClick={()=>{
-              this.onClickList(item)
-            }}>
-              <td>
-              {item.address}
-              </td>
-              <td>
-                {item.title}
-              </td>
-            </tr>
-          })}
-          </tbody>
-        </table>
-      </div>
-    </IconNovelReadWrapper>
-    </div>
+    );
   }
 }
 
--- src/dAppSrc/IconNovelWrite.jsx
+++ src/dAppSrc/IconNovelWrite.jsx
@@ -1,27 +1,23 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
-import uuid from 'uuid/v1'
+import uuid from 'uuid/v1';
 import BasicInput from './components/Input';
 import IconConnect from './components/IconConnect';
 import IconSDK from './components/icon-sdk';
-import {
-  IconConverter
-} from 'icon-sdk-js'
-
+import { IconConverter } from 'icon-sdk-js';
 
 const IconNovelWriteWrraper = styled.div`
-  .write-title-container{
-
+  .write-title-container {
   }
-  .write-content-container{
-    margin-top:30px;
-    margin-bottom:30px;
-    textarea::placeholder{
-      font-size:13px;
-      color:#b3b3b3;
+  .write-content-container {
+    margin-top: 30px;
+    margin-bottom: 30px;
+    textarea::placeholder {
+      font-size: 13px;
+      color: #b3b3b3;
     }
-    textarea{
-      box-sizing:border-box;
+    textarea {
+      box-sizing: border-box;
       resize: none;
       display: block;
       width: 100%;
@@ -33,35 +29,35 @@ const IconNovelWriteWrraper = styled.div`
       border-image: initial;
       padding: 20px;
       &:focus {
-        border: 2px solid rgb(0,188,212);
+        border: 2px solid rgb(0, 188, 212);
         outline: none;
-     }
+      }
     }
   }
-  .write-button{
-    display:flex;
-    text-align:center;
-    font-size:16px;
-    background-color:rgb(0,188,212);
-    color:white;
-    height:40px;
-    line-height:40px;
-    border: 1px solid rgb(0,188,212);
-    border-radius:4px;
-    justify-content:center;
+  .write-button {
+    display: flex;
+    text-align: center;
+    font-size: 16px;
+    background-color: rgb(0, 188, 212);
+    color: white;
+    height: 40px;
+    line-height: 40px;
+    border: 1px solid rgb(0, 188, 212);
+    border-radius: 4px;
+    justify-content: center;
     cursor: pointer;
     transition: 0.5s all;
     position: relative;
-    &:hover{
-      color:rgb(238,238,238);
-      font-weight:bold;
-      font-size:18px;
+    &:hover {
+      color: rgb(238, 238, 238);
+      font-weight: bold;
+      font-size: 18px;
     }
-    &:focus{
-      color:black;
+    &:focus {
+      color: black;
     }
-    &:after{
-      content: "";
+    &:after {
+      content: '';
       background: white;
       display: block;
       position: absolute;
@@ -70,89 +66,92 @@ const IconNovelWriteWrraper = styled.div`
       margin-left: 0px;
       margin-top: 0;
       opacity: 0;
-      transition: all 0.4s
+      transition: all 0.4s;
     }
     &:active:after {
       padding: 0;
       margin: 0;
       opacity: 1;
-      transition: 0s
+      transition: 0s;
     }
   }
-`
+`;
 
 class IconNovelWrite extends Component {
-  constructor(props){
-    super(props)
+  constructor(props) {
+    super(props);
     this.state = {
-      title:'',
-      content:''
-    }
+      title: '',
+      content: '',
+    };
   }
-  onChangeTitle=(e)=>{
-    this.setState({title:e.target.value})
-  }
-  onKeyDownTitle=(e)=>{}
-  onChangeContent=(e)=>{
+  onChangeTitle = e => {
+    this.setState({ title: e.target.value });
+  };
+  onKeyDownTitle = e => {};
+  onChangeContent = e => {
     this.setState({
-      content:e.target.value
-    })
-  }
-  writeValidate=()=>{
-    const {address} = this.props;
-    const {title, content} = this.state
-    if(address ===''){
-      alert('로그인이 필요합니다.')
-      return false
-    } else if(title === ''){
-      alert('제목을 입력해주세요')
-      return false
-    } else if(content === ''){
-      alert('내용을 입력해주세요')
-      return false
+      content: e.target.value,
+    });
+  };
+  writeValidate = () => {
+    const { address } = this.props;
+    const { title, content } = this.state;
+    if (address === '') {
+      alert('로그인이 필요합니다.');
+      return false;
+    } else if (title === '') {
+      alert('제목을 입력해주세요');
+      return false;
+    } else if (content === '') {
+      alert('내용을 입력해주세요');
+      return false;
     }
-    return true
-  }
-  onClickWrite=async()=>{
-    if(this.writeValidate() === false){
-      return
+    return true;
+  };
+  onClickWrite = async () => {
+    if (this.writeValidate() === false) {
+      return;
     }
-    //write 버튼 클릭
-    const { sendTxBuild } = IconSDK;
-    const {address} = this.props;
-    const {title, content} = this.state;
-    
-    let msg = `${title}${window.SPLITER}${content}${window.SPLITER}${address}${window.SPLITER}${uuid()}`
-    console.log('msg', msg)
-    console.log('IconConverter.fromUtf8(msg)', IconConverter.fromUtf8(msg))
-    const txObj = sendTxBuild({
-      methodName:'addWriting',
-      params:{
-        msg:IconConverter.fromUtf8(msg) 
-      },
-      from: address,
-      to: window.CONTRACT_ADDRESS,
-    })
+    try {
+      //write 버튼 클릭
+      const { sendTxBuild } = IconSDK;
+      const { address } = this.props;
+      const { title, content } = this.state;
 
+      let msg = `${title}${window.SPLITER}${content}${
+        window.SPLITER
+      }${address}${window.SPLITER}${uuid()}`;
+      console.log('msg', msg);
+      console.log('IconConverter.fromUtf8(msg)', IconConverter.fromUtf8(msg));
+      const txObj = sendTxBuild({
+        methodName: 'addWriting',
+        params: {
+          msg: IconConverter.fromUtf8(msg),
+        },
+        from: address,
+        to: window.CONTRACT_ADDRESS,
+      });
 
-    console.log('txObj' , txObj)
-    // from: address,
-    const tx = await IconConnect.sendTransaction(txObj)
-    console.log('tx', tx)
-    window.open(`https://tracker.icon.foundation/transaction/${tx}`)
+      console.log('txObj', txObj);
+      // from: address,
+      const tx = await IconConnect.sendTransaction(txObj);
+      console.log('tx', tx);
+      window.open(`https://tracker.icon.foundation/transaction/${tx}`);
 
-    this.setState({
-      title:'',
-      content:''
-    })
-
-  }
+      this.setState({
+        title: '',
+        content: '',
+      });
+    } catch (error) {}
+  };
   render() {
-    const {title, content} = this.state
+    const { title, content } = this.state;
     return (
       <IconNovelWriteWrraper>
         <div className="write-title-container">
-          <BasicInput label={'Title'}
+          <BasicInput
+            label={'Title'}
             inputName="write-title"
             onChangeText={this.onChangeTitle}
             onKeyDown={this.onKeyDownTitle}
@@ -162,14 +161,18 @@ class IconNovelWrite extends Component {
           />
         </div>
         <div className="write-content-container">
-          <textarea placeholder="Please write" onChange={this.onChangeContent} value={content} maxLength="500"></textarea>
+          <textarea
+            placeholder="Please write"
+            onChange={this.onChangeContent}
+            value={content}
+            maxLength="500"
+          />
         </div>
         <div className="write-button-container">
           <div className="write-button" onClick={this.onClickWrite}>
             Write
           </div>
         </div>
-        
       </IconNovelWriteWrraper>
     );
   }
